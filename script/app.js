dab00def934e0d1944b698dbecb42f9e8b88123e
Kader <settahkader@gmail.com>
2020-07-18 12:40:13 +0100

    finishing the header and the navigation style

--- script/app.js
+++ script/app.js
@@ -1,6 +1,12 @@
 const hamburgerBtn = document.querySelector('.hamburger-btn');
 
 hamburgerBtn.addEventListener('click', (e) => {
-  hamburgerBtn.firstElementChild.classList.toggle('is-open');
-  console.log(e.target);
+  const btnClassName = hamburgerBtn.firstElementChild.classList;
+  const mainNav = document.querySelector('.main-nav');
+  btnClassName.toggle('is-open');
+  if (btnClassName.contains('is-open')) {
+    mainNav.style.display = 'block';
+  } else {
+    mainNav.style.display = 'none';
+  }
 });
