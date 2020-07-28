ab21d9ff00cec6deeb2762abb95067af57e99766
Kader <settahkader@gmail.com>
2020-07-28 06:41:01 +0100

    add error msg to the shorten url input

--- script/app.js
+++ script/app.js
@@ -1,3 +1,5 @@
+// display hamburger button
+
 const hamburgerBtn = document.querySelector('.hamburger-btn');
 
 hamburgerBtn.addEventListener('click', (e) => {
@@ -8,5 +10,20 @@ hamburgerBtn.addEventListener('click', (e) => {
     mainNav.style.display = 'block';
   } else {
     mainNav.style.display = 'none';
+  }
+});
+
+// add class to input if the user don't write the url
+const inputBtn = document.querySelector('.input-group__submit');
+inputBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const input = document.querySelector('.input-group__input');
+  const errorMsg = document.querySelector('.input-group__input--error');
+  if (!input.value) {
+    input.classList.add('input-group__input--not-valid');
+    errorMsg.style.display = 'block';
+  } else {
+    input.classList.remove('input-group__input--not-valid');
+    errorMsg.style.display = 'none';
   }
 });
