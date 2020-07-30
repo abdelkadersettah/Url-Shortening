1aa06de7830e99dd047c9e60eb3540eb0371d4cf
Kader <settahkader@gmail.com>
2020-07-30 15:37:45 +0100

    add api script to get short link

--- script/app.js
+++ script/app.js
@@ -25,5 +25,37 @@ inputBtn.addEventListener('click', (e) => {
   } else {
     input.classList.remove('input-group__input--not-valid');
     errorMsg.style.display = 'none';
+    const shortenLink = fetchNewLink(input.value);
+    shortenLink.then((data) => renderShortLink(data));
+    input.value = '';
   }
 });
+
+//get shorten link
+
+function postLink(input) {
+  return fetch('https://rel.ink/api/links/', {
+    method: 'POST',
+    body: JSON.stringify({
+      url: input,
+    }),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  }).then((response) => response.json());
+}
+
+async function fetchNewLink(url) {
+  let newLinkJson = await postLink(url);
+  return newLinkJson;
+}
+function renderShortLink(data) {
+  const outputGroup = document.querySelector('.output-group');
+  outputGroup.innerHTML += `
+  <ul class="output-group__list">
+      <li class="output-group__item">${data.url}</li>
+      <li class="output-group__item">https://rel.ink/${data.hashid}</li>
+      <li class="output-group__cta output-group__cta--copied btn btn--secondary">copy</li>
+  </ul> 
+    `;
+}
